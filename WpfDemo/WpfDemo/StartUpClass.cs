586c324b50ca0a82efa54f18a2acc3f2e5f08b3a
hszilu1020 <hszilu1020@163.com>
2020-07-11 18:26:42 +0800

    add WpfDemo

---
+++ WpfDemo/WpfDemo/StartUpClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfDemo
+{
+    class StartUpClass
+    {
+        /// <summary>
+        /// Application Entry Point.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            new App().Run();
+        }
+    }
+}
